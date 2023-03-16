b9a47f47e294ebdb5be63a412f4a20754fdec3a0
Alexander Frick <Alex313031@gmail.com>
2023-03-15 20:52:44 -0500

    sse4

--- build.sh
+++ build.sh
@@ -21,6 +21,7 @@ displayHelp () {
 	printf "${bold}${GRE}Script to build Geany-ng on Linux.${c0}\n" &&
 	printf "${bold}${YEL}Use the --clean flag to run \`make clean\` & \`make distclean\`.${c0}\n" &&
 	printf "${bold}${YEL}Use the --debug flag to make a debug build.${c0}\n" &&
+	printf "${bold}${YEL}Use the --sse4 flag to make an SSE4.1 build.${c0}\n" &&
 	printf "${bold}${YEL}Use the --help flag to show this help.${c0}\n" &&
 	printf "\n"
 }
@@ -33,7 +34,7 @@ makeClean () {
 	printf "\n" &&
 	printf "${YEL}Running \`make clean\` and \`make distclean\`...\n" &&
 	printf "${CYA}\n" &&
-	make clean && make distclean &&]
+	make clean && make distclean &&
 	printf "\n" &&
 	printf "${GRE}${bold}Done.\n" &&
 	printf "\n" &&
@@ -43,16 +44,49 @@ case $1 in
 	--clean) makeClean; exit 0;;
 esac
 
+buildSSE41 () {
+printf "\n" &&
+printf "${YEL}Building Geany-ng (SSE4.1 Version)...\n" &&
+printf "${CYA}\n" &&
+
+# Build geany-ng
+export CFLAGS="-g0 -s -O3 -msse4.1 -flto=auto -DNDEBUG" &&
+export CXXFLAGS="-g0 -s -O3 -msse4.1 -flto=auto -DNDEBUG" &&
+export CPPFLAGS="-g0 -s -O3 -msse4.1 -flto=auto -DNDEBUG" &&
+export LFLAGS="-Wl,-O3 -msse4.1 -s -flto=auto" &&
+export LDLIBS="-Wl,-O3 -msse4.1 -s -flto=auto" &&
+export LDFLAGS="-Wl,-O3 -msse4.1 -s -flto=auto" &&
+export OPT_LEVEL="3" &&
+export RUSTFLAGS="-C opt-level=3 -C target-feature=+sse4.1" &&
+
+./autogen.sh &&
+
+./configure --enable-the-force &&
+
+make VERBOSE=1 V=1 &&
+
+printf "\n" &&
+printf "${GRE}${bold}Build Completed. ${YEL}${bold}You can now run \`sudo make install\` or \`make install\` to install it.\n" &&
+printf "\n" &&
+tput sgr0
+}
+case $1 in
+	--sse4) buildSSE41; exit 0;;
+esac
+
 printf "\n" &&
 printf "${YEL}Building Geany-ng...\n" &&
 printf "${CYA}\n" &&
-
 
 # Build geany-ng
 export CFLAGS="-g0 -s -O3 -mavx -maes -flto=auto -DNDEBUG" &&
 export CXXFLAGS="-g0 -s -O3 -mavx -maes -flto=auto -DNDEBUG" &&
 export CPPFLAGS="-g0 -s -O3 -mavx -maes -flto=auto -DNDEBUG" &&
-export LDFLAGS="-Wl,-O3 -mavx -maes -flto=auto" &&
+export LFLAGS="-Wl,-O3 -mavx -maes -s -flto=auto" &&
+export LDLIBS="-Wl,-O3 -mavx -maes -s -flto=auto" &&
+export LDFLAGS="-Wl,-O3 -mavx -maes -s -flto=auto" &&
+export OPT_LEVEL="3" &&
+export RUSTFLAGS="-C opt-level=3 -C target-feature=+avx,+aes" &&
 
 ./autogen.sh &&
 
